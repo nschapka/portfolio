7384ee01cc8ef2669f93be1ed478560566cee17c
nschapka <38693762+nschapka@users.noreply.github.com>
2019-04-06 22:54:38 -0400

    Update MetadataScraper.py

--- metaData/MetadataScraper.py
+++ metaData/MetadataScraper.py
@@ -16,26 +16,30 @@ def vals(line):
 
 
 def getChunks(lines):
+    # init arrays
     chunks = []
     chunkTemp = []
     for line in lines:
-        if line != '\n':
+        if line != '\n':                # pulling lines until we reach a blank line - which should just be a newLine char
             chunkTemp.append(line)
-        else:
-            if chunkTemp != []:
+        else:                           # otherwise we terminate the chunk and reset the temp array
+            if chunkTemp != []:         # conditioning against whitespace at the end of the file, or extra lines between chunks
                 chunks.append(chunkTemp)
             chunkTemp = []
 
-    return chunks
+    return chunks # as an array of string arrays
 
 
 with open('C:\\users\\nschapka\\desktop\\mediainfoOut.txt', "r", encoding="utf-8") as f:
     lines = []
+    # pull the whole text in at once
     for line in f:
         lines.append(line)
 
 
+# for each chunk given by getChunks we grab a title as a key, and a subDict as a value, populated from each following line of the chunk
 thisDict = {title(chunk): {keys(line): vals(line) for line in chunk[1:len(chunk)+1]} for chunk in getChunks(lines)}
 
+# test outputs
 print(thisDict)
 print(thisDict['General']['Format'])
